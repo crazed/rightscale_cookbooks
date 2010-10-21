27e11ff34c7fbbaf5fbf166f9a7b8c4113e0e06a
Allan <cr4z3d@gmail.com>
2010-10-21 20:43:12 +0000

    working on getting s3cmd to work

--- s3cmd/metadata.rb
+++ s3cmd/metadata.rb
@@ -1,6 +1,19 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Datapipe"
+maintainer_email "afeid@datapipe.com"
 license          "All rights reserved"
 description      "Installs/Configures s3cmd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+recipe "s3cmd::default", "Installs and configures s3cmd"
+attribute	'aws_access_key',
+  :display_name => "AWS Access Key",
+  :description => "Your AWS access key",
+  :type => "string",
+  :required => "required",
+  :recipes => [ 's3cmd::default' ]
+attribute	'aws_secret_key',
+  :display_name => "AWS Secret Key",
+  :description => "Your AWS Secret Key",
+  :type => "string",
+  :required => "required",
+  :recipes => [ 's3cmd::default' ]
--- s3cmd/recipes/default.rb
+++ s3cmd/recipes/default.rb
@@ -8,19 +8,12 @@
 #
 
 package "s3cmd"
-begin
-  aws_config = data_bag_item(node.default[:client_name], "aws")
-  Chef::Log.info("Loaded AWS information from DataBagItem #{node.default[:client_name]}[#{aws_config['id']}]")
-rescue
-  Chef::Log.fatal("Could not find the 'aws' item in the '#{node.default[:client_name]}' data bag")
-  raise
-end
 
-template "/etc/s3cfg.conf" do
+template ".s3cfg" do
   source "s3cfg.erb"
-  variables(:passphrase => aws_config['s3_passphrase'], 
-    :aws_secret_access_key_id => aws_config['aws_secret_access_key_id'],
-    :aws_access_key_id => aws_config['aws_access_key_id'])
+  variables(
+    :aws_secret_access_key_id => node.aws_secret_key,
+    :aws_access_key_id => node.aws_access_key)
   owner "root"
   group "root"
   mode 600
