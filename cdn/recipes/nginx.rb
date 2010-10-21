2d483c2307913262f443b53e6d3e28ce755cc2bd
Allan <cr4z3d@gmail.com>
2010-10-21 17:43:42 +0000

    added new default nginx.conf and modified proxy.conf

--- cdn/recipes/nginx.rb
+++ cdn/recipes/nginx.rb
@@ -5,6 +5,19 @@ service "nginx" do
   action [ :enable ]
 end
 
+directory "/cache" do
+  owner "www-data"
+  group "www-data"
+  mode "0755"
+end
+
+remote_file "/etc/nginx/nginx.conf" do
+  owner "root"
+  group "root"
+  mode "644"
+  source "nginx.conf"
+end
+
 template "/etc/nginx/conf.d/proxy.conf" do
   owner "root"
   group "root"
