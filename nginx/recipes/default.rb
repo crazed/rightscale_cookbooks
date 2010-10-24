5f544446614471631bf1bd25785ea3b408d7ca9a
crazed <cr4z3d@gmail.com>
2010-10-24 20:50:44 +0000

    prettier looking variables line

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -30,9 +30,10 @@ end
 # default nginx configuration
 template "/etc/nginx/sites-available/default" do
   source "nginx-default.erb"
-  variables (
+  variables(
     :document_root => doc_root,
-    :server_name => node.domain_name)
+    :server_name => node.domain_name
+  )
 end
 
 # nginx.conf, set the processes to total cpu cores
