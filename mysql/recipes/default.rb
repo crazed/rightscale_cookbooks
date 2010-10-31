0647cb99e301d6e66a2c00b76c20ae35267363b6
crazed <cr4z3d@gmail.com>
2010-10-31 18:21:47 +0000

    actually setting the rootpw now

--- mysql/recipes/default.rb
+++ mysql/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: mysql
 # Recipe:: default
 #
-# Copyright 2010, YOUR_COMPANY_NAME
+# Copyright 2010, Datapipe
 #
 # All rights reserved - Do Not Redistribute
 #
@@ -10,5 +10,27 @@
 package "mysql-server"
 
 service "mysql" do
-  action :start
+  action [ :start, :enable ]
+end
+
+execute "set root pw" do
+  command "mysql < /tmp/rootpw.sql && rm -f /tmp/rootpw.sql"
+  action :nothing
+end
+
+template "/tmp/rootpw.sql" do
+  source "sql.erb"
+  owner "root"
+  group "root"
+  mode "600"
+  variables(:password => node.mysql.rootpw)
+  notifies :run, resources(:execute => "set root pw"), :immediately
+end
+
+template "/root/.my.cnf" do
+  source "my-cnf.erb"
+  owner "root"
+  group "root"
+  mode "600"
+  variables(:user => "root", :password => node.mysql.rootpw)
 end
