073f811e2f93c4d96bdcea2b77945feabc0b8c25
crazed <cr4z3d@gmail.com>
2010-11-02 16:38:29 +0000

    hm still cant get template to render

--- rails/recipes/nginx.rb
+++ rails/recipes/nginx.rb
@@ -2,13 +2,20 @@ package "nginx"
 service "nginx" do
   action :stop
 end
+
+sockets = Array.new
+for i in (0..(node.rails.thin_servers-1))
+  sockets << "/tmp/thin.#{i}.sock"
+end
+
 template "/etc/nginx/conf.d/thin.conf" do
   source "thin.conf.erb"
-  variables(:thin_sockets => node.rails.thin_servers)
+  variables(:thin_sockets => sockets)
   owner "root"
   group "root"
   mode "644"
 end
+
 template "/etc/nginx/sites-available/default" do
   source "nginx-default.erb"
   variables(:app_path => node.rails.app_path)
