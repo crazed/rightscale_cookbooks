8f71151fab54fc8d9bdc52cd6a93034de3059cef
crazed <cr4z3d@gmail.com>
2010-11-02 16:15:15 +0000

    make sure certain directories exists for deploy, also remove the tmp symlink

--- rails/recipes/deploy.rb
+++ rails/recipes/deploy.rb
@@ -1,10 +1,22 @@
 package "git-core"
+directory "#{node.rails.app_path}/system" do
+  owner "root"
+  group "root"
+  mode "755"
+end
+directory "#{node.rails.app_path}/log"do
+  owner "root"
+  group "root"
+  mode "755"
+end
 deploy node.rails.app_path do
   repo node.rails.repo
   migrate true
   migration_command "rake db:migrate"
   shallow_clone true
   environment "HOME" => "/tmp", "RAILS_ENV" => "production"
+  symlinks "system" => "public/system", "log" => "log"
+  restart_command "thin restart --all /etc/thin"
   action :deploy
 end
 
