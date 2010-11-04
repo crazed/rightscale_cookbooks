57152b5e2b38b0dfb03b18e33c7916f1be2bccb8
crazed <cr4z3d@gmail.com>
2010-11-04 18:44:51 +0000

    moved to passenger as main script

--- rails/recipes/rollback.rb
+++ rails/recipes/rollback.rb
@@ -5,7 +5,7 @@ deploy node.rails.app_path do
   migration_command "rake gems:install db:migrate"
   shallow_clone true
   environment "HOME" => "/tmp", "RAILS_ENV" => node.rails.environment
-  restart_command "thin restart --all /etc/thin"
+  restart_command "service nginx reload"
   action :rollback
 end
 
