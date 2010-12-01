418d280089ced840abb429b50ce54fc4d50a7eb1
crazed <cr4z3d@gmail.com>
2010-12-01 16:29:32 +0000

    rails 2.3.8 is not rack based, need to use unicorn_rails

--- rails/recipes/unicorn.rb
+++ rails/recipes/unicorn.rb
@@ -28,9 +28,13 @@ end
 
 service "unicorn" do
   supports [ :start, :stop, :reload, :restart ]
-  start_command "unicorn -c /etc/unicorn/unicorn.rb -E #{node.rails.environment} -D"
+  #start_command "unicorn -c /etc/unicorn/unicorn.rb -E #{node.rails.environment} -D"
+  # rails 2.3.8 is not Rack
+  start_command "unicorn_rails -c /etc/unicorn/unicorn.rb -E #{node.rails.environment} -D"
   stop_command "kill `cat /var/run/unicorn.pid`"
   reload_command "kill -USR2 `cat /var/run/unicorn.pid`"
-  restart_command "kill `cat /var/run/unicorn.pid` && unicorn -c /etc/unicorn -E #{node.rails.environment} -D"
+  # rails 2.3.8 is not Rack
+  #restart_command "kill `cat /var/run/unicorn.pid` && unicorn -c /etc/unicorn -E #{node.rails.environment} -D"
+  restart_command "kill `cat /var/run/unicorn.pid` && unicorn_rails -c /etc/unicorn -E #{node.rails.environment} -D"
   action :start
 end
