3b51da3aa4856219ebc43906f3ff35b23dd906b4
crazed <cr4z3d@gmail.com>
2010-12-22 17:08:14 +0000

    fixing maintenance page locations

--- rails/recipes/maintenance.rb
+++ rails/recipes/maintenance.rb
@@ -1,10 +1,10 @@
 execute "control maintenance page" do
-  maintenance_path = "#{node.rails.app_path}/shared/system/maintenance.html"
+  maintenance_path = "#{node.rails.app_path}/current/public/system/maintenance.html"
   if File.exists(maintenance_path)
     Chef::Log.info "Maintenance page is currently active, disabling it."
     command "rm -f #{maintenance_path}"
   else
     Chef::Log.info "Maintenance page is not active, enabling it."
-    command "ln -s #{node.rails.app_path}/shared/system/#{node.rails.maintenance_page} #{maintenance_path}"
+    command "ln -s #{node.rails.app_path}/current/public/system/#{node.rails.maintenance_page} #{maintenance_path}"
   end
 end
