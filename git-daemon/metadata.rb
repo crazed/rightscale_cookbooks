d6e8cff2e6b67e92c3aeaad124dac69eb8861e3f
Allan <cr4z3d@gmail.com>
2011-01-14 17:05:13 -0500

    fixing metadata again

--- git-daemon/metadata.rb
+++ git-daemon/metadata.rb
@@ -4,6 +4,7 @@ license          "All rights reserved"
 description      "Installs/Configures git-daemon and gitweb"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+recipe "git-daemon::default", "installs and configures git-daemon and gitweb"
 attribute 'git/export_path',
   :display_name => "Git Export Path",
   :description => "Path that will be exported by git-daemon",
