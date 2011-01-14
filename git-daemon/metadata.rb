dc113c20233227fc49a022ca8f2a13f6f94152be
Allan <cr4z3d@gmail.com>
2011-01-14 16:55:28 -0500

    added git-daemon cookbook

---
+++ git-daemon/metadata.rb
@@ -0,0 +1,12 @@
+maintainer       "Datapipe"
+maintainer_email "afeid@datapipe.com"
+license          "All rights reserved"
+description      "Installs/Configures git-daemon and gitweb"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+attribute 'git/export_path',
+  :display_name => "Git Export Path",
+  :description => "Path that will be exported by git-daemon",
+  :type => "string"
+  :default => "/srv/git"
+  :recipes => [ 'git-daemon::default' ]
---
+++ git-daemon/recipes/default.rb
@@ -0,0 +1,39 @@
+#
+# Cookbook Name:: git-daemon
+# Recipe:: default
+#
+# Copyright 2011, Datapipe
+#
+# All rights reserved - Do Not Redistribute
+#
+package apache2
+package git
+package git-daemon-run
+package gitweb
+
+directory node.git.export_path do
+  owner 'gitdaemon'
+  action :create
+end
+
+template '/etc/sv/git-daemon/run' do
+  owner 'root'
+  mode '644'
+  template 'git-daemon-run.erb'
+  variables(:git_dir => node.git.export_path)
+end
+
+template '/etc/gitweb.conf' do
+  owner 'root'
+  mode '644'
+  template 'gitweb.conf.erb'
+  variables(:git_dir => node.git.export_path)
+end
+
+service 'git-daemon' do
+  start_command "sv start git-daemon"
+  stop_command "sv stop git-daemon"
+  status_command "sv status git-daemon"
+  restart_command "sv restart git-daemon"
+  action :restart
+end
