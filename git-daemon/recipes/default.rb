5eeff9dc2f6f9cf19c24c7d9ecfc34b86c85d813
Allan <cr4z3d@gmail.com>
2011-01-17 10:41:42 -0500

    fixing some errors

--- git-daemon/recipes/default.rb
+++ git-daemon/recipes/default.rb
@@ -18,15 +18,15 @@ end
 
 template '/etc/sv/git-daemon/run' do
   owner 'root'
-  mode '644'
-  template 'git-daemon-run.erb'
+  mode '755'
+  source 'git-daemon-run.erb'
   variables(:git_dir => node.git.export_path)
 end
 
 template '/etc/gitweb.conf' do
   owner 'root'
   mode '644'
-  template 'gitweb.conf.erb'
+  source 'gitweb.conf.erb'
   variables(:git_dir => node.git.export_path)
 end
 
@@ -34,6 +34,6 @@ service 'git-daemon' do
   start_command "sv start git-daemon"
   stop_command "sv stop git-daemon"
   status_command "sv status git-daemon"
-  restart_command "sv restart git-daemon"
+  restart_command "sv restart git-daemon; true" # will return 1, though it restarts fine
   action :restart
 end
