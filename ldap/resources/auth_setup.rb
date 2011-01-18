94448da59f42a64b074e4e814c0a528e1475c6e5
Allan <cr4z3d@gmail.com>
2011-01-18 15:57:41 -0500

    removed base_dn as required

--- ldap/resources/auth_setup.rb
+++ ldap/resources/auth_setup.rb
@@ -1,5 +1,5 @@
 actions :install
-attribute :base_dn, :kind_of => String, :name_attribute => true, :required => true
+attribute :base_dn, :kind_of => String, :name_attribute => true
 attribute :bind_dn, :kind_of => String, :required => true
 attribute :bind_pw, :kind_of => String, :required => true
 attribute :ldap_uri, :kind_of => String, :required => true
