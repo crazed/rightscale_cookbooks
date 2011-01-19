06b62b33834d8dd2099bd8eb26cbd81263b27172
Allan <cr4z3d@gmail.com>
2011-01-19 16:47:37 -0500

    attempting to fix hostname issues

--- utils/recipes/set_hostname.rb
+++ utils/recipes/set_hostname.rb
@@ -2,4 +2,24 @@ if node.utils.hostname
   utils_set_hostname node.utils.hostname do
     action :enable
   end
+
+  # find the ipv4 address of the server
+  ipv4 = nil
+  node.network.interfaces[node.network.default_interface].addresses.each do |address|
+    if address.family == 'inet'
+      ipv4 = address
+      Chef::Log.info("Found IP Address: #{ipv4}")
+    end
+  end
+
+  case ipv4
+  when nil
+    Chef::Log.fatal('Could not find and IPv4 Address')
+    raise
+  else
+    execute 'add to hosts file' do
+      not_if "grep #{ipv4} /etc/hosts"
+      command "echo #{ipv4} >> /etc/hosts"
+    end
+  end
 end
