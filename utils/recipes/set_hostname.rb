b34599d0930ff56540153a5a62494e28d7229d1b
Allan <cr4z3d@gmail.com>
2011-01-19 16:57:00 -0500

    debugging

--- utils/recipes/set_hostname.rb
+++ utils/recipes/set_hostname.rb
@@ -6,21 +6,21 @@ if node.utils.hostname
   # find the ipv4 address of the server
   ipv4 = nil
   node.network.interfaces[node.network.default_interface].addresses.each do |address|
-    Chef::Log.info("Address: #{address[0]}")
     if address[1]['family'] == 'inet'
-      ipv4 = address
+      ipv4 = address[0]
       Chef::Log.info("Found IP Address: #{ipv4}")
     end
   end
 
   case ipv4
   when nil
-    Chef::Log.fatal('Could not find and IPv4 Address')
+    Chef::Log.fatal('Could not find an IPv4 Address')
     raise
   else
     execute 'add to hosts file' do
-      not_if "grep #{ipv4} /etc/hosts"
-      command "echo #{ipv4} >> /etc/hosts"
+      hosts_line = "#{ipv4} #{node.utils.hostname}"
+      not_if "grep #{hosts_line} /etc/hosts"
+      command "echo #{hosts_line} >> /etc/hosts"
     end
   end
 end
