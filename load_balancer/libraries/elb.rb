508464ebbb9009431a3aa68d862261d6ebb0cbca
Allan <cr4z3d@gmail.com>
2011-01-20 14:15:24 -0500

    typo issue

--- load_balancer/libraries/elb.rb
+++ load_balancer/libraries/elb.rb
@@ -8,7 +8,7 @@ module Opscode
   module Aws
     module Elb
       def elb
-        @@elb ||= FOG::AWS::ELB.new(:aws_access_key_id => new_resource.access_key_id, :aws_secret_access_key => new_resource.secret_access_key)
+        @@elb ||= Fog::AWS::ELB.new(:aws_access_key_id => new_resource.access_key_id, :aws_secret_access_key => new_resource.secret_access_key)
       end
     end
   end
