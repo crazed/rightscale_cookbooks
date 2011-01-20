6db93d74f885ab513df995c4d0b179a6bd0a1268
Allan <cr4z3d@gmail.com>
2011-01-20 14:16:40 -0500

    fixed variable names

--- load_balancer/libraries/elb.rb
+++ load_balancer/libraries/elb.rb
@@ -8,7 +8,7 @@ module Opscode
   module Aws
     module Elb
       def elb
-        @@elb ||= Fog::AWS::ELB.new(:aws_access_key_id => new_resource.access_key_id, :aws_secret_access_key => new_resource.secret_access_key)
+        @@elb ||= Fog::AWS::ELB.new(:aws_access_key_id => new_resource.access_key, :aws_secret_access_key => new_resource.secret_key)
       end
     end
   end
