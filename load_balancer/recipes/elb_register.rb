f497abb3aed8e376257069c700cb5989b75359a4
Allan <cr4z3d@gmail.com>
2011-01-20 13:02:41 -0500

    removed load_balancer cookbook for now

--- load_balancer/recipes/elb_register.rb
+++
@@ -1,6 +0,0 @@
-include_recipe 'load_balancer::default'
-load_balancer_elb node.elb.name do
-  access_key node.aws.access_key
-  secret_key node.aws.secret_key
-  action :register
-end
--- load_balancer/resources/elb.rb
+++
@@ -1,4 +0,0 @@
-actions :register
-attribute :access_key, :kind_of => String, :required => true
-attribute :secret_key, :kind_of => String, :required => true
-attribute :elb_name, :kind_of => String, :name_attribute => true
