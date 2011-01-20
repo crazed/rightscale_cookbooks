4455c7267e650a7304a878013e6aaf54a1112aec
Allan <cr4z3d@gmail.com>
2011-01-20 14:07:39 -0500

    added library files

--- load_balancer/libraries/elb.rb
+++ load_balancer/libraries/elb.rb
@@ -4,3 +4,12 @@ rescue LoadError
   Chef::Log.warn("Missing gem 'fog'")
 end
 
+module Opscode
+  module Aws
+    module Elb
+      def elb
+        @@elb ||= FOG::AWS::ELB.new(:aws_access_key_id => new_resource.access_key_id, :aws_secret_access_key => new_resource.secret_access_key)
+      end
+    end
+  end
+end
