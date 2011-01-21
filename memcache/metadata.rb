96ed50b8d41c36183f4569c389535168a0b0db11
Allan <cr4z3d@gmail.com>
2011-01-21 16:20:59 -0500

    memcache cookbook almost ready

--- memcache/metadata.rb
+++ memcache/metadata.rb
@@ -1,7 +1,24 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Datapipe"
+maintainer_email "cloud@datapipe.com"
 license          "All rights reserved"
 description      "Installs/Configures memcache"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 recipe "memcache::default", "installs and configured memcached"
+attribute 'memcache/memory_size',
+  :display_name => 'Memcache Max Memory',
+  :description => 'Maximum memory allocation for memcache in megabytes',
+  :type => 'string',
+  :default => '64',
+  :recipes => [ 'memcache::default' ]
+attribute 'memcache/bind_address',
+  :display_name => 'Memcache Bind Address',
+  :description => 'The address to listen on for memcached.',
+  :type => 'string',
+  :default => '127.0.0.1',
+  :recipes => [ 'memcache::default' ]
+attribute 'memcache/port',
+  :display_name => 'Memcache Port',
+  :description => 'Port for memcache to bind to',
+  :default => '11211',
+  :recipes => [ 'memcache::defualt' ]
--- memcache/recipes/default.rb
+++ memcache/recipes/default.rb
@@ -6,8 +6,21 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package "memcached"
+package 'memcached'
 
-service "memcached" do
-  action [ :enable, :start ]
+service 'memcached' do
+  running true
+end
+
+template '/etc/memcached.conf' do
+  owner 'root'
+  group 'root'
+  mode '644'
+  source 'memcache.conf.erb'
+  variables(
+    :listen => node.memcache.bind_address,
+    :memory_size => node.memcache.memory_size,
+    :port => node.memcache.port
+  )
+  notifies :restart, resources(:service => 'memcached'), :immediately
 end
