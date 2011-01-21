ab7f1ac316b117e68e5d76acb45193dffea593e6
Allan <cr4z3d@gmail.com>
2011-01-21 14:12:05 -0500

    more elegant way of clearing paths

--- load_balancer/recipes/default.rb
+++ load_balancer/recipes/default.rb
@@ -2,6 +2,11 @@ package 'build-essential'
 package 'ruby-dev'
 package 'libxml2-dev'
 package 'libxslt1-dev'
+
 gem_package 'fog' do
   gem_binary '/opt/rightscale/sandbox/bin/gem'
+end
+
+ruby_block 'clear paths' do
+  block { Gem.clear_paths }
 end
