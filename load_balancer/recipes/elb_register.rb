6b22dc09bd5210bd4fcea9761a5413ad6086cfe7
Allan <cr4z3d@gmail.com>
2011-01-21 13:08:40 -0500

    trying to fix gem loading

--- load_balancer/recipes/elb_register.rb
+++ load_balancer/recipes/elb_register.rb
@@ -1,4 +1,7 @@
 include_recipe 'load_balancer::default'
+gem_package 'fog' do
+  gem_binary '/opt/rightscale/sandbox/bin/gem'
+end
 load_balancer_elb node.elb.name do
   access_key node.aws.access_key
   secret_key node.aws.secret_key
