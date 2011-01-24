36846faa29c06074e5a2607d95160c6f7bbdad46
Allan <cr4z3d@gmail.com>
2011-01-24 10:32:08 -0500

    making sure that rubygems use the official sources rather than rightscales mirror

--- rails/recipes/ruby.rb
+++ rails/recipes/ruby.rb
@@ -13,6 +13,11 @@ package "libopenssl-ruby"
 package "build-essential"
 package 'rubygems'
 
+# the rightscale gem sources seem to miss some updates
+# and also have broken gem specs at times, add the default ones
+# to avoid problems
+execute "gem sources -a http://gems.rubyforge.org"
+
 gem_package "rails" do
   version "2.3.8"
 end
