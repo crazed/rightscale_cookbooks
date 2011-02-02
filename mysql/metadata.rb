04fc0eeffccdbfe6d800a8fac0a2bd0c2146531b
Allan <cr4z3d@gmail.com>
2011-02-02 15:51:48 -0500

    added new_database recipe

--- mysql/metadata.rb
+++ mysql/metadata.rb
@@ -5,6 +5,18 @@ description      "Installs/Configures mysql"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 recipe "mysql::default", "Installs mysql-server"
+recipe "mysql::new_database", "Configures a new database"
 attribute 'mysql/rootpw',
   :display_name => "MySQL Root Password",
-  :required => "required"
+  :required => "required",
+  :recipes => [ 'mysql::default' ]
+attribute 'mysql/user',
+  :display_name => 'MySQL User',
+  :description => 'A regular mysql user, used typically for application level database access.',
+  :default => 'user',
+  :recipes => [ 'mysql::new_database' ]
+attribute 'mysql/password',
+  :display_name => 'MySQL User Password',
+  :description => 'The password for a regular MySQL user.',
+  :required => 'required',
+  :recipes => [ 'mysql::new_database' ]
