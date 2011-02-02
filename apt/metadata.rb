c54a5ca1abacd6c3d6cf0bdbaf15b5e3d70c73b6
Allan <cr4z3d@gmail.com>
2011-02-02 16:49:13 -0500

    added new auto remove recipe

--- apt/metadata.rb
+++ apt/metadata.rb
@@ -5,3 +5,4 @@ description      "Installs/Configures apt"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 recipe "apt::default", "configures apt to not install recommended or suggested packages"
+recipe "apt::auto_remove", "runs apt-get autoremove -y"
---
+++ apt/recipes/auto_remove.rb
@@ -0,0 +1,1 @@
+execute "apt-get autoremove -y"
