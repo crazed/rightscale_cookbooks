372629a2f791e9776f8e0ffe3bafe7d6c669ef7b
Allan <cr4z3d@gmail.com>
2011-03-07 21:59:51 -0500

    deploy code as new user

--- devops/recipes/cruisecontrol.rb
+++ devops/recipes/cruisecontrol.rb
@@ -5,8 +5,18 @@
 
 package "openssl-ruby"
 package "rubygems"
+
 gem_package "rake"
+
+user "builder"
+
+directory "/srv" do
+  owner 'builder'
+  group 'builder'
+end
+
 git "/srv/cruisecontrol" do
+  user "builder"
   repository "git://github.com/thoughtworks/cruisecontrol.rb.git"
   action :sync
 end
