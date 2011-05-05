7c4f879e33c0e4b2e0afb68dfbdf33bb7d41fc11
Allan <cr4z3d@gmail.com>
2011-05-05 12:08:00 -0400

    fixed some metadata

--- s3cmd/metadata.rb
+++ s3cmd/metadata.rb
@@ -7,6 +7,25 @@ version          "0.0.1"
 recipe "s3cmd::default", "Installs and configures s3cmd"
 recipe "s3cmd::sync", "Syncs an s3 bucket path to a specific directory"
 recipe "s3cmd::pull", "Pulls down data from an s3 bucket to a specific directory"
+recipe "s3cmd::backup", "Backs up a filesystem path to and S3 bucket"
+attribute "s3.backup_bucket",
+  :display_name => "S3 Backup Bucket",
+  :description => "Enter the name of the bucket you would like to backup direcotries to",
+  :type => "string",
+  :required => "required",
+  :recipes => [ 's3cmd::backup' ]
+attribute "s3.bucket_path",
+  :display_name => "S3 Bucket Path",
+  :description => "The path in your S3 bucket to store backup files",
+  :type => "string",
+  :required => "required",
+  :recipes => [ 's3cmd::backup' ]
+attribute "s3.backup_path",
+  :display_name => "Backup Path",
+  :description => "The full path you would like backed up to S3, this will be tar gzipped",
+  :type => "string",
+  :required => "required",
+  :recipes => [ 's3cmd::backup' ]
 attribute	'aws_access_key',
   :display_name => "AWS Access Key",
   :description => "Your AWS access key",
