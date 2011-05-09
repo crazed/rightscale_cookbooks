548e844a6d11a238fd66b9efcc57aa847a87cd85
Allan <cr4z3d@gmail.com>
2011-05-09 10:52:03 -0400

    templating the redis configuration, added additional attributes

--- redis/metadata.rb
+++ redis/metadata.rb
@@ -5,3 +5,19 @@ description      "Installs/Configures redis"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 recipe "redis::install", "installs/configures redis"
+attribute 'redis/bind_address',
+  :display_name => 'Redis Bind Address',
+  :description => 'Address to listen on for Redis',
+  :default => '127.0.0.1',
+  :recipes => [ 'redis::install' ]
+attribute 'redis/listen_port',
+  :display_name => 'Redis Bind Port',
+  :description => 'The port Redis should listen on',
+  :default => '6379',
+  :recipes => [ 'redis::install' ]
+attribute 'redis/loglevel',
+  :display_name => 'Redis Log Level',
+  :description => 'Log level to set for Redis',
+  :choice => [ 'debug', 'verbose', 'notice', 'warning' ],
+  :default => 'notice',
+  :recipes => [ 'redis::install' ]
--- redis/recipes/install.rb
+++ redis/recipes/install.rb
@@ -8,3 +8,21 @@
 #
 
 package "redis-server"
+
+service "redis" do
+  running true
+end
+
+template "/etc/redis/redis.conf" do
+  source "redis.conf.erb"
+  owner "root"
+  group "root"
+  mode "644"
+  variables(
+    :bind_addr => node.redis.bind_address,
+    :port => node.redis.listen_port,
+    :loglevel => node.redis.loglevel,
+    :num_databases => node.redis.databases
+  )
+  notifies :restart, resources(:service => 'redis'), :immediately
+end
